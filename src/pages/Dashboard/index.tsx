0d1675a8b20410855bca3215a6f806b67bfd0827
herbertdesousa <sousaherbert138@gmail.com>
2020-09-06 15:22:06 -0300

    chore: uncomment

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -32,40 +32,40 @@ const Dashboard: React.FC = () => {
   const { addToCart } = useCart();
 
   const [products, setProducts] = useState<Product[]>([
-    {
-      id: '123',
-      title: 'Nome do produto 01',
-      image_url:
-        'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
-      price: 50,
-    },
-    {
-      id: '1234',
-      title: 'Nome do produto 02',
-      image_url:
-        'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
-      price: 60,
-    },
-    {
-      id: '12345',
-      title: 'Nome do produto 03',
-      image_url:
-        'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
-      price: 70,
-    },
-    {
-      id: '123456',
-      title: 'Nome do produto 04',
-      image_url:
-        'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
-      price: 80,
-    },
+    // {
+    //   id: '123',
+    //   title: 'Nome do produto 01',
+    //   image_url:
+    //     'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+    //   price: 50,
+    // },
+    // {
+    //   id: '1234',
+    //   title: 'Nome do produto 02',
+    //   image_url:
+    //     'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+    //   price: 60,
+    // },
+    // {
+    //   id: '12345',
+    //   title: 'Nome do produto 03',
+    //   image_url:
+    //     'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+    //   price: 70,
+    // },
+    // {
+    //   id: '123456',
+    //   title: 'Nome do produto 04',
+    //   image_url:
+    //     'https://storage.googleapis.com/golden-wind/bootcamp-gostack/camiseta-ecommerce.jpg',
+    //   price: 80,
+    // },
   ]);
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // const response = await api.get('/products');
-      // setProducts(response.data);
+      const response = await api.get('/products');
+      setProducts(response.data);
     }
 
     loadProducts();
